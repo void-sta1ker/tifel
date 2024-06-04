ccfb3bb0aac85eb794b5dd2394627abe4db88b19
void-sta1ker <iamdilshodboy@gmail.com>
2024-06-04 17:38:08 +0500

    build: add acorn

--- src/types/ast.ts
+++ src/types/ast.ts
@@ -1,32 +1,58 @@
 interface Node {
   type: string;
-  value: string;
-  loc: {
-    start: {
-      line: number;
-      column: number;
-    };
-    end: {
-      line: number;
-      column: number;
-    };
-  };
+  start: number;
+  end: number;
   [key: string]: any;
 }
 
-interface Ast {
+interface Ast extends Node {
   type: "Program";
   body: Node[];
+  sourceType: "script" | "module";
 }
 
-interface CallExpression extends Node {
-  type: "CallExpression";
+interface Expression extends Node {}
+
+interface Statement extends Node {}
+
+interface ConditionalExpression extends Expression {
+  type: "ConditionalExpression";
+  test: Node;
+  consequent: Node;
+  alternate: Node;
+}
+
+interface Literal extends Node {
+  type: "Literal";
+  value: string;
+  raw: string;
+  regex?: { pattern: string; flags: string };
+}
+
+interface UnaryExpression extends Expression {
+  type: "UnaryExpression";
+  operator: string;
+  prefix: boolean;
+  argument: Node;
+}
+
+interface VariableDeclarator extends Node {
+  type: "VariableDeclarator";
+  id: Node;
+  init: Node;
+}
+
+interface Identifier extends Node {
+  type: "Identifier";
   name: string;
-  arguments: Node[];
-  // callee: Node;
-  // parent: Node;
 }
 
-export type { Node };
+interface VariableDeclaration extends Node {
+  type: "VariableDeclaration";
+  declarations: VariableDeclarator[];
+  kind: "var" | "let" | "const";
+}
 
 export default Ast;
+
+export type { Node };
