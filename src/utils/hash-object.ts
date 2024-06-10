43c5c05da90291f4ea2724d99706ec306761ad86
void-sta1ker <iamdilshodboy@gmail.com>
2024-06-10 16:50:37 +0500

    chore: add hashing object client side & replace hashing with serializing

--- src/utils/hash-object.ts
+++ src/utils/hash-object.ts
@@ -1,4 +1,6 @@
-import crypto from "crypto";
+function isClient() {
+  return typeof window !== "undefined";
+}
 
 function serializeObj(obj: object) {
   return JSON.stringify(obj, (key, value) => {
@@ -7,6 +9,25 @@ function serializeObj(obj: object) {
   });
 }
 
-export default function hashObject(obj: object) {
+// Hash the serialized object using the Node.js crypto module
+async function hashObjectNode(obj: object) {
+  const crypto = await import("crypto");
   return crypto.createHash("sha256").update(serializeObj(obj)).digest("hex");
 }
+
+// Hash the serialized object using the Web Crypto API
+async function hashObjectWeb(obj: object) {
+  const serialized = serializeObj(obj);
+  const encoder = new TextEncoder();
+  const data = encoder.encode(serialized);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+  return hashHex;
+}
+
+// export default isClient() ? hashObjectWeb : hashObjectNode;
+
+export default serializeObj;
